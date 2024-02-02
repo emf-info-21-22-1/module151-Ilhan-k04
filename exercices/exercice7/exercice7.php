f5f6085d2d67c7e9afd79547907d7d2a55bbaad4
ilhan-k04 <ilhankaynak2004@gmail.com>
2024-02-02 13:25:32 +0100

    exercice 7 fait

--- exercices/exercice7/exercice7.php
+++ exercices/exercice7/exercice7.php
@@ -3,9 +3,9 @@ $bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
 $reponse = $bdd->query("SELECT titre FROM jeux_video");
 
 
-while (…)
-{
-
+while ($donnees = $reponse->fetch()) {
+	echo $donnes['titre'] . '<br>';
 }
 $reponse->closeCursor();
+
 ?>
