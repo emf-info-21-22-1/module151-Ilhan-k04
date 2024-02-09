cb4559b2879fb4b65ed3a5ade77dd6afd72dd0c7
ilhan-k04 <ilhankaynak2004@gmail.com>
2024-02-09 08:25:04 +0100

    v1 ex8

---
+++ exercices/exercice8/serveur/main.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+include_once('ctrl/EquipeCtrl.php');
+include_once('ctrl/JoueurCtrl.php');
+
+switch ($_GET['action']) {
+    case 'equipe':
+        $equipes = new EquipeCtrl();
+        echo $equipes->getEquipesInXML();
+        break;
+    case 'joueur':
+        $joueurs = new JoueurCtrl();
+        echo $joueurs->getJoueursInXML($_GET['equipeId']);
+        break;
+}
